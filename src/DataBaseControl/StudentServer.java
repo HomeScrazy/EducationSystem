aba84a0f869967fb34c0351b1907ce0f1cdcb656
Holmes <crazy_runcheng@163.com>
2015-07-22 09:42:16 +0800

    exist checking

--- src/DataBaseControl/StudentServer.java
+++ src/DataBaseControl/StudentServer.java
@@ -5,28 +5,36 @@ import java.sql.*;
 import Model.Student;
 
 public class StudentServer {
+	
+	private static final String SQL_URL="jdbc:mysql://localhost:3306/EducationSystem?probileSQL=true&useUnicode=true&characterEncoding=GBK";
+	private static final String USER="root";
+	private static final String PASSWORD="root";
+	
 	public void InsertStudent(Student student){
 		try{
 			Class.forName("com.mysql.jdbc.Driver").newInstance();
-			String url="jdbc:mysql://localhost:3306/EducationSystem?probileSQL=true&useUnicode=true&characterEncoding=GBK";
-			String user="root";
-			String pwd="root";
-			Connection conn=DriverManager.getConnection(url, user, pwd);
+			Connection conn=DriverManager.getConnection(SQL_URL, USER, PASSWORD);
 			Statement stm=conn.createStatement();
-			StringBuilder sb=new StringBuilder();
-			sb.append("insert into Students values('");
-			sb.append(student.GetAccount());
-			sb.append("','");
-			sb.append(student.GetName());
-			sb.append("','");
-			sb.append(student.GetSex());
-			sb.append("','");
-			sb.append(student.GetClassName());
-			sb.append("','");
-			sb.append(student.GetLocation());
-			sb.append("')");
-			System.out.println(sb.toString());
-			stm.execute(sb.toString());
+			if(stm.execute("select * from Students where Id='"+student.GetAccount()+"'")){
+				return;
+			}
+			else{
+				StringBuilder sb=new StringBuilder();
+				sb.append("insert into Students values('");
+				sb.append(student.GetAccount());
+				sb.append("','");
+				sb.append(student.GetName());
+				sb.append("','");
+				sb.append(student.GetSex());
+				sb.append("','");
+				sb.append(student.GetClassName());
+				sb.append("','");
+				sb.append(student.GetLocation());
+				sb.append("')");
+				System.out.println(sb.toString());
+				stm.execute(sb.toString());
+			}
+			
 		}catch(Exception e){
 			e.printStackTrace();
 		}
